3f231b3182b383174fbc08d991ec4aba47f230b7
ibrahimTN <chrif.brahim@ymail.com>
2021-01-13 22:54:18 +0100

    exercice1.html

--- web/tp/tables.php
+++ web/tp/tables.php
@@ -34,12 +34,9 @@
             <li class="nav-item">
               <a class="nav-link " href="formulaire.html">Formulaires</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link " href="exercice1.html">Exercice1</a>
-            </li>
           </ul>
           <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" id="myInput" aria-label="Search">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
         </div>
@@ -56,7 +53,7 @@
             <th scope="col">Handle</th>
           </tr>
         </thead>
-        <tbody id="myTable">
+        <tbody>
           <tr class="table-danger">
             <td scope="row">1</td>
             <td class="bg-primary">Mark</td>
@@ -157,16 +154,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script>
-      $(document).ready(function(){
-        $("#myInput").on("keyup", function() {
-          var value = $(this).val().toLowerCase();
-          $("#myTable tr").filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-          });
-        });
-      });
-      </script>
   </body>
 </html>
 
